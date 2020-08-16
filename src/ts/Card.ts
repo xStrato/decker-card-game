6df6a61f998f09507b47f6d232be93e18aa3525b
xStrato <metalofgear@hotmail.com>
2020-08-16 00:31:11 -0300

    feat: refactoring Card class, added flip method with animation and functions for face-down and face-up

--- src/ts/Card.ts
+++ src/ts/Card.ts
@@ -1,7 +1,13 @@
-import { Geom, GameObjects, Scene } from "phaser";
+import { Geom, GameObjects, Scene, Types, Display } from "phaser";
 
+const { Color, Graphics, Group, Text  } = {...Display, ...GameObjects}
 const { Rectangle } = Geom
-const { Group } = GameObjects
+
+export enum CardState
+{
+    FACE_DOWN,
+    FACE_UP,
+}
 
 type CardDimensions = {
     x: number
@@ -16,9 +22,9 @@ type CardData = {
     color: string
 }
 
-interface ICard<T extends GameObjects.Group | GameObjects.Container>
+interface ICard
 {
-    card: T
+    // card: T
     width: number
     height: number
     x: number
@@ -28,18 +34,19 @@ interface ICard<T extends GameObjects.Group | GameObjects.Container>
 
 export default class Card extends GameObjects.Container
 {
-    public card: GameObjects.Container;
+    // public card: GameObjects.Container;
     public width: number
     public height: number
     public x: number
     public y: number
-    private textConfig: {}
-
+    private textConfig: Types.GameObjects.Text.TextStyle
+    private cardState: CardState
+    private faceData: CardData
     /*
     *
     * @param {CardData} - Naipes: 1 - clubs (♣), 2 - diamonds (♦), 3 - hearts (♥), 4 - spades (♠)
     */
-    constructor(scene: Scene, dimensions: CardDimensions, data: CardData )
+    constructor(scene: Scene, dimensions: CardDimensions, data: CardData, cardState: CardState = CardState.FACE_DOWN, flipAnimation: boolean = true)
     {
         super(scene)
         const { x, y, width, height } = dimensions
@@ -48,61 +55,95 @@ export default class Card extends GameObjects.Container
       this.height = height
       this.x = x
       this.y = y
-      this.textConfig = { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', color: data.color, fontSize: Math.floor(width * .2) }
+      this.textConfig = { fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif', color: data.color, fontSize: `${Math.floor(width * .2)}` }
+      this.cardState = cardState
+      this.faceData = data
 
-      this.draw({...data})
+      this.flip(cardState, flipAnimation)
+
+      this.scene.add.existing(this)
     }
-
-    get ref(): GameObjects.Container
-    {
-        return this.card
-    }
-
-    public draw(data: CardData): void
+    
+    private faceUp(data: CardData): void
     {
         let {naipe, cardNumber, color} = data
 
         naipe = this.getCardNaipe(naipe)
         cardNumber = this.getCardNumber(cardNumber)
 
-        this.card = this.scene.add.container()
-        const layout = this.scene.add.graphics()
-            .fillRectShape(new Rectangle(0, 0, this.width, this.height))
-            .fillStyle(0xFFFFFF)
+        const layout = new Graphics(this.scene).fillRectShape(new Rectangle(0, 0, this.width, this.height)).fillStyle(0xFFFFFF)
 
-        const text1 = this.scene.add.text(5, 0, cardNumber, this.textConfig);
-        const text2 = this.scene.add.text(this.width - text1.width-3, 0, cardNumber, this.textConfig);
-        const text3 = this.scene.add.text(text1.width+5, this.height, cardNumber, this.textConfig).setAngle(180);
-        const text4 = this.scene.add.text(this.width-3, this.height, cardNumber, this.textConfig).setAngle(180);
+        const text1 = new Text(this.scene, 5, 0, cardNumber, this.textConfig);
+        const text2 = new Text(this.scene, this.width - text1.width-3, 0, cardNumber, this.textConfig);
+        const text3 = new Text(this.scene, text1.width+5, this.height, cardNumber, this.textConfig).setAngle(180);
+        const text4 = new Text(this.scene, this.width-3, this.height, cardNumber, this.textConfig).setAngle(180);
 
         //Creates the naipe symbols under the numbers
-        const text1Symbol1 = this.scene.add.text(0, 0, naipe, this.textConfig)
+        const text1Symbol1 = new Text(this.scene, 0, 0, naipe, this.textConfig)
         text1Symbol1.setPosition(text1Symbol1.width/2 - text1.width *.1, text1.height/2)
-        const text1Symbol2 = this.scene.add.text((this.width - text1Symbol1.width) - text1.width * .1, text1.height/2, naipe, this.textConfig)
-        const text1Symbol3 = this.scene.add.text(text1Symbol1.width+text1.width * .2, this.height-text1.height/2, naipe, this.textConfig).setAngle(180)
-        const text1Symbol4 = this.scene.add.text(this.width-text1.width *.1, this.height-text1.height/2, naipe, this.textConfig).setAngle(180)
+        const text1Symbol2 = new Text(this.scene, (this.width - text1Symbol1.width) - text1.width * .1, text1.height/2, naipe, this.textConfig)
+        const text1Symbol3 = new Text(this.scene, text1Symbol1.width+text1.width * .2, this.height-text1.height/2, naipe, this.textConfig).setAngle(180)
+        const text1Symbol4 = new Text(this.scene, this.width-text1.width *.1, this.height-text1.height/2, naipe, this.textConfig).setAngle(180)
 
         //Creates main symbol
-        let symbol = this.scene.add.text(0,0, naipe, this.textConfig).setFontSize(this.width)
+        let symbol = new Text(this.scene, 0,0, naipe, this.textConfig).setFontSize(this.width)
         symbol.setPosition(this.width/2-symbol.width/2, this.height/2 - symbol.height/2)
         symbol = this.ajustNaipeSize(symbol);
 
-        const centerText = this.scene.add.text(0,0, cardNumber, this.textConfig).setFontSize(this.width/2.5).setColor("#fff")
+        const centerText = new Text(this.scene, 0,0, cardNumber, this.textConfig).setFontSize(this.width/2.5).setColor("#fff")
         centerText.setPosition(this.width/2 - centerText.width/2, this.height/2 - centerText.height/2)
 
-        const rect = this.scene.add.graphics()
+        const rect = new Graphics(this.scene)
         rect.lineStyle(1, color !== '#000'? 0xff0000 : 0x000, 1)
         rect.strokeRect(0, 0, this.width/1.5, this.height/1.5)
         //Centralizar a 1/6 to tamanho total da carta
         rect.setPosition(this.width/6, this.height/6)
 
-        this.card.add([layout, text1, text2, text3, text4, text1Symbol1, text1Symbol2, text1Symbol3, text1Symbol4, symbol, centerText, rect])
-        this.card.setPosition(this.x, this.y)
+        this.add([layout, text1, text2, text3, text4, text1Symbol1, text1Symbol2, text1Symbol3, text1Symbol4, symbol, centerText, rect])
+        this.setPosition(this.x, this.y)
+    }
+
+    private faceDown(color: number): void
+    {
+        const layout = new Graphics(this.scene, {fillStyle: { color: 0xffffff } })
+            .fillRectShape(new Rectangle(0, 0, this.width, this.height))
+
+        const squadSize = this.width/15
+        const cellSize = 7
+
+        const diamond = new Graphics(this.scene, {fillStyle: { color } }).fillRectShape(new Rectangle(0, 0, squadSize, squadSize))
+
+        const textureName = `diamond${Math.floor(Math.random()*100000)}`
+        diamond.generateTexture(textureName)
+
+        const alignConfig: 
+        Types.Actions.GridAlignConfig = 
+        {
+            width: this.width/cellSize,
+            height: this.height/cellSize,
+            cellWidth: cellSize,
+            cellHeight: cellSize,
+            x: 5,
+            y: 6
+        }
+
+        const groupConfig: 
+        Types.GameObjects.Group.GroupCreateConfig = 
+        {
+            key: textureName,
+            frameQuantity: (this.width/7) * Math.floor(this.height/7),
+            gridAlign: alignConfig
+        }
+
+        const group = new Group(this.scene, groupConfig)
+        this.add([layout,...group.getChildren()])
+        group.destroy()
+        layout.closePath()
     }
 
     private ajustNaipeSize(naipeText: GameObjects.Text): GameObjects.Text
     {
-        const ref = this.scene.add.text(0,0, "♥", this.textConfig).setFontSize(this.width).setVisible(false)
+        const ref = new Text(this.scene, 0,0, "♥", this.textConfig).setFontSize(this.width).setVisible(false)
         naipeText.width = ref.width
         ref.destroy()
 
@@ -134,7 +175,25 @@ export default class Card extends GameObjects.Container
         }
     }
 
-    public update(): void
+    public flip(cardState: CardState, animation: boolean = true): void
     {
+        if(animation)
+        {
+            this.scene.tweens.add({
+                targets: this,
+                x: this.x + 10,
+                y: this.y + 10,
+                duration: 400,
+                ease: 'Power2',
+                yoyo: true
+            })
+        }
+        
+        switch(cardState)
+        {
+            case CardState.FACE_DOWN: this.faceDown(Color.HexStringToColor(this.faceData.color).color); break;
+            case CardState.FACE_UP: this.faceUp({...this.faceData}); break;
+            default: break;
+        }
     }
   }
--- src/ts/Main.ts
+++ src/ts/Main.ts
@@ -1,96 +1,99 @@
-import { Scene, GameObjects } from "phaser";
-import Card from "./Card";
+import { Scene } from "phaser";
+import Card, { CardState } from "./Card";
 
 export default class Main extends Scene
 {
   private height: number
   private width: number
   public mainDeck: Card
-  public deck1: Array<GameObjects.Container> = [];
-  public deck2: Array<GameObjects.Container> = [];
+  public deck1: Array<Card> = [];
+  public deck2: Array<Card> = [];
   public paddingX = 120;
   public paddingLeft = 25;
   public paddingY = 20;
   public widthCard = 70
   public heightCard = 100
+  public spreadNumber: number
 
-    constructor()
+    constructor(spreadNumber: number)
     {
-      super("Main") 
+      super("Main")
+      this.spreadNumber = 5
     }
 
     public preload(): void
     {
       this.width = +this.game.config.width
       this.height = +this.game.config.height
-      this.initSetup()
+      this.initSetup(this.spreadNumber)
     }
 
     public create(): void
     {
-      const card = new Card(this, {x: this.width-this.widthCard, y: this.height/2 - this.heightCard/2+this.paddingY, width:this.widthCard, height:this.heightCard}, {naipe: `${Phaser.Math.Between(1, 4)}`, cardNumber: `${Phaser.Math.Between(1, 13)}`, color: '#FF0000' })
+      const card = new Card(this, {x: this.width-this.widthCard, y: this.height/2 - this.heightCard/2+this.paddingY, width:this.widthCard, height:this.heightCard}, {naipe: `${Phaser.Math.Between(1, 4)}`, cardNumber: `${Phaser.Math.Between(1, 13)}`, color: this.setColor() }, undefined, false)
+      card.setAngle(90)
 
-      card.ref.setRotation(90*Math.PI/180)
+      this.spreadCards(this.deck1, this.paddingX, this.height-this.heightCard-this.paddingY, (card: Card) => card.flip(CardState.FACE_UP))
+      this.spreadCards(this.deck2, this.paddingX, this.paddingY)
 
-      this.revealDeck(this.deck1, this.paddingX, this.height-this.heightCard-this.paddingY)
-      this.revealDeck(this.deck2, this.paddingX, this.paddingY)
-      
+      this.time.delayedCall(8000, () => this.deck2[Phaser.Math.Between(0, 4)].flip(CardState.FACE_UP))
     }
 
-    public initSetup(): void
+    private initSetup(spreadNumber: number): void
     {
-      for (let i = 0; i < 5; i++)
-      {
-        this.add.graphics()
-        .lineStyle(1, 0x000, 1)
-        .strokeRect(this.paddingLeft+this.paddingX*i, this.paddingY, this.widthCard, this.heightCard)
-      }
-
-      for (let i = 0; i < 5; i++)
-      {
-        this.add.graphics()
-        .lineStyle(1, 0x000, 1)
-        .strokeRect(this.paddingLeft+this.paddingX*i, this.height-this.heightCard-this.paddingY, this.widthCard, this.heightCard)
-      }
-
-      for (let i = 0; i < 5; i++)
-      {
-        this.tweens.add({
-          targets: this.deck1[i],
-          x: (this.paddingLeft+this.paddingX)*i,
-          y: this.height-this.heightCard-this.paddingY,
-          duration: 3000,
-          ease: 'Power2',
-          angle: 0
-        })
-      }
-      for (let i = 0; i < 5; i++)
-      {
-        const card = new Card(this, {x:this.width-this.widthCard, y:this.height/2 - this.heightCard/2+this.paddingY, width:this.widthCard, height:this.heightCard}, {naipe: `${Phaser.Math.Between(1, 4)}`, cardNumber: `${Phaser.Math.Between(1, 13)}`, color: '#FF0000' })
-        this.deck1.push(card.ref.setRotation(90*Math.PI/180));
-      }
-
-      for (let i = 0; i < 5; i++)
-      {
-        const card = new Card(this, {x: this.width-this.widthCard, y:this.height/2 - this.heightCard/2+this.paddingY, width:this.widthCard, height:this.heightCard}, {naipe: `${Phaser.Math.Between(1, 4)}`, cardNumber: `${Phaser.Math.Between(1, 13)}`, color: '#000' })
-        this.deck2.push(card.ref.setRotation(90*Math.PI/180));
-      }
+      this.setupCardBoardStrokes(spreadNumber)
+      this.setupCardForPlayers(spreadNumber)
     }
 
-    public revealDeck(deck: GameObjects.Container[], x: number, y: number): void
+    private spreadCards(deck: Card[], x: number, y: number, callback?: any): void
     {
-      this.time.addEvent({delay: 1000})
-      for (let i = 0; i < 5; i++)
-      {
+      //Starts the spread for players with hang out animation
+      deck.forEach((card, index) => {
+
         this.tweens.add({
-          targets: deck[i],
-          x: this.paddingLeft+x*i,
+          targets: card,
+          x: this.paddingLeft+x*index,
           y,
-          duration: 3000,
+          duration: 2000,
           ease: 'Power2',
-          angle: 0
+          angle: 0,
+          delay: 1000*index,
+          onComplete: () => this.time.delayedCall(50*index, callback, [card]),
         })
+      })
+    }
+
+    private setupCardForPlayers(spreadNumber: number)
+    {
+      for (let i = 0; i < spreadNumber; i++)
+      {
+        //Generates cards for both players
+        const cardDeck1 = new Card(this, {x:this.width-this.widthCard, y:this.height/2 - this.heightCard/2+this.paddingY, width:this.widthCard, height:this.heightCard}, {naipe: `${Phaser.Math.Between(1, 4)}`, cardNumber: `${Phaser.Math.Between(1, 13)}`, color: this.setColor() }).setAngle(90)
+        
+        const cardDeck2 = new Card(this, {x:this.width-this.widthCard, y:this.height/2 - this.heightCard/2+this.paddingY, width:this.widthCard, height:this.heightCard}, {naipe: `${Phaser.Math.Between(1, 4)}`, cardNumber: `${Phaser.Math.Between(1, 13)}`, color: this.setColor() }).setAngle(90)
+
+        //Add cards into array
+        this.deck1.push(cardDeck1);
+        this.deck2.push(cardDeck2);
       }
+    }
+
+    private setupCardBoardStrokes(spreadNumber: number)
+    {
+      for (let i = 0; i < spreadNumber; i++)
+      {
+        //Draw card strokes in the board
+        this.drawCardStroke(this.paddingLeft+this.paddingX*i, this.height-this.heightCard-this.paddingY, this.widthCard, this.heightCard)
+
+        this.drawCardStroke(this.paddingLeft+this.paddingX*i, this.paddingY, this.widthCard, this.heightCard)
+      }
+    }
+
+    private setColor = (): string => Phaser.Math.Between(0,1) === 1 ? '#FF0000' : '#000'
+
+    private drawCardStroke(x: number, y: number, w: number, h:number): void
+    {
+      this.add.graphics().lineStyle(1, 0x000, 1).strokeRect(x, y, w, h)
     }
 
     public update(): void
